93c2a8aa1c48e6542a3f0895042f43d1f1229fde
Adib Hanna <adibhanna@gmail.com>
2021-06-14 05:11:29 -0700

    Tailwind + Webpack improvements (#26)
    * Tailwind + Webpack improvements
    
    - Add TailwindCSS
    - Live reload
    
    * remove webpack.HotModuleReplacementPlugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../styles/main.css'
 import App from './components/App'
+
+require('file-loader?name=[name].[ext]!../index.html');
 
 const root = document.getElementById('app-root');
 
--- webpack.common.cjs
+++ webpack.common.cjs
@@ -1,27 +1,56 @@
 const path = require('path');
 const webpack = require('webpack');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const rules = [
   {
-    test:/\.tsx?/,
+    test: /\.tsx?/,
     exclude: /node_modules/,
-    loader:'ts-loader'
-  }
-]
+    loader: 'ts-loader',
+  },
+  {
+    test: /\.(css)$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      'css-loader', 
+      {
+        loader: 'postcss-loader', 
+        options: {
+          postcssOptions: {
+            ident: 'postcss',
+            plugins: [tailwindcss, autoprefixer],
+          },
+        },
+      },
+    ],
+  },
+];
+
 module.exports = {
-  target:'web',
-  entry:'./src/index.tsx',
+  target: 'web',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
-    path:path.resolve(".", 'dist'),
-    filename:'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   plugins: [
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
-    })],
-  module: {rules},
-  resolve:{
-    extensions:['.ts', '.tsx', '.js'],
-    fallback: { "crypto": require.resolve("crypto-browserify"), "stream": require.resolve("stream-browserify"), "buffer": require.resolve("buffer") }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].css'
+    }),
+  ],
+  module: { rules },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer'),
+    },
   },
-}
+};
