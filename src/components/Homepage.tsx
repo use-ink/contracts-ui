7b7e93db2fdc9d2988573a6b84d30eee37d7763c
Adib Hanna <adibhanna@gmail.com>
2021-06-17 13:21:05 -0700

    temporarily fix tests

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,11 +4,12 @@ export default function Homepage() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <>
+      <h1>Hello</h1>
       <p>You clicked {count} times..</p>
       <button onClick={() => setCount(count + 1)} className="border px-4 py-2 bg-blue-300 text-gray-850">
         Click me
       </button>
-    </div>
+    </>
   );
 }
