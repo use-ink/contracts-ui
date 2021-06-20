43faaf187183044bc4541cf497c46aae96f6f390
Andreea Eftene <hi@pixelle.ro>
2021-06-20 22:48:17 +0200

    speed up tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   globals: {
     'ts-jest': {
       useESM: true,
