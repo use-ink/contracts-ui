5ca94cb82ed64e63a2af71d4b9d35a63d6d1e446
Keith Ingram <kwingram25@gmail.com>
2021-07-01 09:03:45 -0700

    import error

--- src/ui/hooks/useCodeBundle.ts
+++ src/ui/hooks/useCodeBundle.ts
@@ -1,7 +1,7 @@
 // Copyright 2021 @paritytech/canvas-ui-v2 authors & contributors
 
 import { useCallback } from 'react';
-import { useDatabase } from './useDatabase';
+import { useDatabase } from '../contexts';
 import { useQuery } from './useQuery';
 import { findCodeBundleById  } from '@db/queries';
 
@@ -10,9 +10,9 @@ import type { CodeBundleDocument, UseQuery } from '@db/types';
 export function useCodeBundle(id: string): UseQuery<CodeBundleDocument> {
   const { db } = useDatabase();
 
-  const findContract = useCallback((): Promise<CodeBundleDocument | null> => {
+  const query = useCallback((): Promise<CodeBundleDocument | null> => {
     return findCodeBundleById(db, id);
   }, [id, findCodeBundleById]);
 
-  return useQuery(findContract);
+  return useQuery(query);
 }
