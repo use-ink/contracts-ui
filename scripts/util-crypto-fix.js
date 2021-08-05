465fb06d2d53dfd4776fc8e03a302749e11bf50f
Keith Ingram <kwingram25@gmail.com>
2021-08-05 15:01:11 -0700

    fix version conflicts

--- scripts/util-crypto-fix.js
+++ scripts/util-crypto-fix.js
@@ -8,9 +8,8 @@ function applyHDKDFix (libName) {
   // const secp256k1Path = path.resolve('node_modules/@polkadot/util-crypto/secp256k1/deriveHard.js')
 
   fs.readFile(filePath, 'utf8', function (err,data) {
-    console.log(data);
     if (err) {
-      return console.log(err);
+      return console.error(err);
     }
     var result = data.replace(
       /\n(const HDKD = compactAddLength\(stringToU8a\('.*'\)\);)\n(export function .*\(seed, chainCode\) {)\n/gm,
@@ -18,7 +17,26 @@ function applyHDKDFix (libName) {
     );
   
     fs.writeFile(filePath, result, 'utf8', function (err) {
-      if (err) return console.log(err);
+      if (err) return console.error(err);
+    });
+  });
+}
+
+function applyNobodyFix () {
+  const filePath = path.resolve(`node_modules/@polkadot/keyring/pair/nobody.js`);
+  // const secp256k1Path = path.resolve('node_modules/@polkadot/util-crypto/secp256k1/deriveHard.js')
+
+  fs.readFile(filePath, 'utf8', function (err,data) {
+    if (err) {
+      return console.error(err);
+    }
+    var result = data.replace(
+      /(import \{ encodeAddress \} from '@polkadot\/util-crypto';\n)((?:.+\n)+)(export function nobody\(\) \{\n)/gm,
+      `$1$3$2`
+    );
+  
+    fs.writeFile(filePath, result, 'utf8', function (err) {
+      if (err) return console.error(err);
     });
   });
 }
@@ -27,6 +45,7 @@ function main() {
   console.log('Correcting util-crypto TextEncoder issue...');
   applyHDKDFix('nacl');
   applyHDKDFix('secp256k1');
+  applyNobodyFix();
 }
 
 main();
