8289e12ba1de7fa3d3145fad79adae670326f491
Adib Hanna <adibhanna@gmail.com>
2021-08-20 12:40:19 -0700

    Instantiate with Code
    WIP

--- src/ui/components/pages/InstantiateWithHash.tsx
+++ src/ui/components/pages/InstantiateWithHash.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { InstantiateWizard } from '../instantiate';
 
-export function InstantiateWithHash () {
+export function InstantiateWithHash() {
   return (
     <div className="w-full mx-auto overflow-y-auto">
       <div className="grid md:grid-cols-12 gap-5 px-5 py-3 m-2">
         <main className="md:col-span-8 p-4">
           <div className="space-y-1 border-b pb-6 dark:border-gray-800 border-gray-200">
-            <h1 className="text-2xl dark:text-white text-gray-700">
-              Instantiate from Code Hash
-            </h1>
+            <h1 className="text-2xl dark:text-white text-gray-700">Instantiate from Code Hash</h1>
             <p className="dark:text-gray-400 text-gray-500 text-sm">
               You can instantiate a new contract from an existing code bundle here.
             </p>
