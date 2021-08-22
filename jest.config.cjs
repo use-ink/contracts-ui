52c4eff6a62d92d7dc41d3252152153477cf7756
Keith Ingram <kwingram25@gmail.com>
2021-08-22 17:03:10 -0500

    unneeded line

--- jest.config.cjs
+++ jest.config.cjs
@@ -15,7 +15,6 @@ module.exports = {
     Uint8Array: Uint8Array,
   },
   moduleNameMapper: {
-    '@polkadot/util-crypto': resolve(__dirname, './node_modules/@polkadot/util-crypto'),
     'styled-components': resolve(__dirname, './node_modules/styled-components/dist/styled-components'),
     '^canvas$': resolve(__dirname, './src/canvas'),
     '^canvas/(.*)$': resolve(__dirname, './src/canvas/$1'),
