e52a65060882265e084c217fc21baaadd3340e58
Keith Ingram <kwingram25@gmail.com>
2021-09-02 01:57:31 -0500

    Fix TypeScript Error errors (#95)

--- src/db/queries/derive.ts
+++ src/db/queries/derive.ts
@@ -1,15 +1,15 @@
 import { getCodeBundleCollection, getContractCollection } from "./util";
-import { findTopCodeBundles } from "./codeBundle";
+// import { findTopCodeBundles } from "./codeBundle";
 import type { Database, DbStatistics } from 'types';
 
 export async function getStatistics (db: Database): Promise<DbStatistics> {
-  try {
+  // try {
     const codeBundles = (await getCodeBundleCollection(db).find({}).toArray());
-    const mostPopularCodeBundles = (await findTopCodeBundles(db)).sort((a, b) => b.instances - a.instances).slice(0, 2);
+    // const mostPopularCodeBundles = (await findTopCodeBundles(db)).sort((a, b) => b.instances - a.instances).slice(0, 2);
     const contractsCount = (await getContractCollection(db).find({}).toArray()).length;
 
-    return { codeBundlesCount: codeBundles.length, contractsCount, mostPopularCodeBundles }
-  } catch (e) {
-    throw new Error(e);
-  }
+    return { codeBundlesCount: codeBundles.length, contractsCount, mostPopularCodeBundles: [] }
+  // } catch (e) {
+  //   throw new Error(e);
+  // }
 }
