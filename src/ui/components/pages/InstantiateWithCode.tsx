ec1b821290d3e838a47c180fd38b8da4b2ff4dd0
Adib Hanna <adibhanna@gmail.com>
2021-09-05 18:18:29 -0700

    revert from use Page

--- src/ui/components/pages/InstantiateWithCode.tsx
+++ src/ui/components/pages/InstantiateWithCode.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Wizard } from '../instantiate';
-import { Page } from 'ui/templates';
 
-export function InstantiateWithCode() {
+export const InstantiateWithCode = () => {
   return (
-    <Page
-      header="Upload and Instantiate Contract"
-      help="You can instantiate a new contract from an existing code bundle here."
-    >
-      <p className="text-gray-200">
+    <div className="w-full max-w-6xl overflow-y-auto px-5 py-3 m-2">
+      <div className="grid md:grid-cols-12 gap-5">
+        <div className="md:col-span-9 py-3 px-4">
+          <div className="space-y-1 border-b pb-6 dark:border-gray-800 border-gray-200">
+            <h1 className="text-2.5xl dark:text-white text-gray-700">
+              Upload and Instantiate Contract
+            </h1>
+            <p className="dark:text-gray-400 text-gray-500 text-sm">
+              You can instantiate a new contract from an existing code bundle{' '}
+              <Link to="/instantiate/hash" className="text-blue-500">
+                here
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-12 gap-5 m-1">
         <Wizard instatiationType="code" />
-      </p>
-    </Page>
+      </div>
+    </div>
   );
-}
+};
--- src/ui/components/pages/InstantiateWithHash.tsx
+++ src/ui/components/pages/InstantiateWithHash.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Wizard } from '../instantiate';
-import { Page } from 'ui/templates';
 
 export const InstantiateWithHash = () => {
   return (
-    <Page
-      header="Upload and Instantiate Contract"
-      help="You can instantiate a new contract from an existing code bundle here."
-    >
-      <Wizard instatiationType="hash" />
-    </Page>
+    <div className="w-full max-w-6xl overflow-y-auto px-5 py-3 m-2">
+      <div className="grid md:grid-cols-12 gap-5">
+        <div className="md:col-span-9 py-3 px-4">
+          <div className="space-y-1 border-b pb-6 dark:border-gray-800 border-gray-200">
+            <h1 className="text-2.5xl dark:text-white text-gray-700">Instantiate from Code Hash</h1>
+            <p className="dark:text-gray-400 text-gray-500 text-sm">
+              You can instantiate a new contract from an existing code bundle{' '}
+              <Link to="/instantiate/code" className="text-blue-500">
+                here
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-12 gap-5 m-1">
+        <Wizard instatiationType="hash" />
+      </div>
+    </div>
   );
 };
