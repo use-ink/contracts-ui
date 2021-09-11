897171557c91d5a8e4021129017eaba6c53e2514
Andreea Eftene <hi@pixelle.ro>
2021-09-11 21:49:51 +0200

    fix loader

--- src/ui/components/contract/Interact.tsx
+++ src/ui/components/contract/Interact.tsx
@@ -85,10 +85,15 @@ export const Interact = ({ metadata, contractAddress, callFn }: Props) => {
 
   if (state.isLoading) {
     return (
-      <>
-        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-16 w-16"></div>
-        <div> Calling instance...</div>
-      </>
+      <div className="w-full h-full fixed flex top-0 left-0 bg-gray-900 opacity-75 z-50">
+        <div className="m-auto flex flex-col justify-center items-center">
+          <div
+            style={{ borderTopColor: 'transparent' }}
+            className="w-16 h-16 border-4 border-blue-400 border-solid rounded-full animate-spin mb-4"
+          ></div>
+          <div> Calling instance...</div>
+        </div>
+      </div>
     );
   }
   return (
