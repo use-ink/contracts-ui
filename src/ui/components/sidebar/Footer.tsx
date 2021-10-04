8330d7d0cc8df82b4666e400720b74c93541f167
erler <mail@michelerler.com>
2021-10-04 12:22:23 +0200

    sidebar font sizes and weights

--- src/ui/components/sidebar/Footer.tsx
+++ src/ui/components/sidebar/Footer.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { ChatAltIcon, CogIcon } from '@heroicons/react/outline';
 
-export function Footer () {
+export function Footer() {
   return (
     <footer className="footer">
       <div>
         <a
           href="#"
-          className="flex content-center text-sm font-medium dark:text-gray-500 text-gray-600 dark:hover:text-gray-300 hover:text-gray-500"
+          className="flex content-center text-xs font-medium dark:text-gray-500 text-gray-600 dark:hover:text-gray-300 hover:text-gray-500"
         >
-          <ChatAltIcon className="h-5 w-5 mr-1" aria-hidden="true" />
+          <ChatAltIcon className="h-4 w-4 mr-1" aria-hidden="true" />
           Help &amp; Feedback
         </a>
         <a href="#">
           <CogIcon
-            className="h-5 w-5 dark:text-gray-500 dark:hover:text-gray-300 text-gray-600 hover:text-gray-500"
+            className="h-4 w-4 dark:text-gray-500 dark:hover:text-gray-300 text-gray-600 hover:text-gray-500"
             aria-hidden="true"
           />
         </a>
       </div>
     </footer>
   );
-};
+}
