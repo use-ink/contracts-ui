40c464af73e2781d634a55e89653c6b288fcdb1a
erler <mail@michelerler.com>
2021-10-05 11:51:24 +0200

    Update Input.tsx

--- src/ui/components/Input.tsx
+++ src/ui/components/Input.tsx
@@ -9,13 +9,13 @@ interface Props extends React.HTMLAttributes<HTMLInputElement> {
 
 export const Input = ({ value, handleChange, placeholder = '' }: Props) => {
   return (
-    <div className="mb-4">
+    <div className="mb-6">
       <input
         type="text"
         placeholder={placeholder}
         value={value}
         onChange={handleChange}
-        className="w-full dark:bg-gray-900 dark:text-gray-300 bg-white dark:border-gray-700 border-gray-200 rounded"
+        className="w-full text-sm dark:bg-gray-900 dark:text-gray-300 bg-white dark:border-gray-700 border-gray-200 rounded"
       />
     </div>
   );
