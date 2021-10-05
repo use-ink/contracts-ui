90afbd9d4569ec6145a9a561ea294df560a23216
erler <mail@michelerler.com>
2021-10-05 11:34:55 +0200

    Update FileInput.tsx

--- src/ui/components/FileInput.tsx
+++ src/ui/components/FileInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { UploadIcon, DocumentIcon, XIcon } from '@heroicons/react/solid';
+import { XIcon } from '@heroicons/react/solid';
+import { DocumentTextIcon, UploadIcon } from '@heroicons/react/outline';
 
 interface Props extends React.HTMLAttributes<HTMLInputElement> {
   fileLoaded?: boolean;
@@ -18,16 +19,16 @@ export const FileInput = ({
 }: Props) => {
   return fileLoaded ? (
     <div className={`${className} flex`}>
-      <div className="p-6 border dark:border-gray-700 border-gray-300 inline-flex items-center rounded">
-        <DocumentIcon
-          className="w-5 h-5 mr-2 text-green-500 justify-self-start"
+      <div className="px-4 py-6 border dark:border-gray-700 border-gray-300 dark:bg-elevation-1 inline-flex items-center rounded">
+        <DocumentTextIcon
+          className="w-5 h-5 mr-2 text-gray-500 justify-self-start"
           aria-hidden="true"
         />
-        <span className="dark:text-gray-300 text-gray-500 text-xs min-w-600 justify-self-start mr-20">
+        <span className="dark:text-gray-300 text-gray-500 text-sm min-w-600 justify-self-start mr-20">
           {successText}
         </span>
         <XIcon
-          className="w-5 h-5 mr-2 text-green-500 justify-self-end cursor-pointer"
+          className="w-5 h-5 text-gray-500 hover:text-gray-300 justify-self-end cursor-pointer"
           aria-hidden="true"
           onClick={removeHandler}
         />
@@ -36,11 +37,11 @@ export const FileInput = ({
   ) : (
     <div className={className}>
       <label
-        className="dark:text-gray-700 text-gray-400 font-bold py-2 px-4 border dark:border-gray-700 border-gray-200 rounded flex flex-col h-36 items-center cursor-pointer justify-center"
+        className="dark:text-gray-700 text-gray-400 font-bold py-6 px-4 space-y-2 border dark:border-gray-700 border-gray-200 rounded flex flex-col items-center cursor-pointer justify-center"
         htmlFor="file"
       >
         <UploadIcon className="h-10 dark:text-gray-500" />
-        <span className="dark:text-gray-500 text-gray-400">{placeholder}</span>
+        <span className="text-sm font-normal dark:text-gray-500 text-gray-400">{placeholder}</span>
       </label>
       <input
         type="file"
