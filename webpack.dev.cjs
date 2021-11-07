4e88c885fc36a8fcfc6641789b79c095635c9596
Andreea Eftene <hi@pixelle.ro>
2021-11-07 13:18:22 +0100

    put back dev mode, remove unused

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -4,21 +4,21 @@ const webpack = require('webpack');
 const common = require('./webpack.common.cjs');
 
 module.exports = merge(common, {
-  mode: 'none',
+  mode: 'development',
   devtool: 'inline-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("development"),
-      }
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+      },
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'dist'),
     },
     compress: true,
     port: 8081,
+    hot: true,
   },
 });
