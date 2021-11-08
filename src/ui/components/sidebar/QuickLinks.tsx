be1e473a3ab6d009ef6e3e3c70d59e3600e98db6
Andreea Eftene <hi@pixelle.ro>
2021-11-08 18:41:28 +0100

    remove starring

--- src/ui/components/sidebar/QuickLinks.tsx
+++ src/ui/components/sidebar/QuickLinks.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { DocumentTextIcon } from '@heroicons/react/outline';
 import { Link } from 'react-router-dom';
 import { NavLink } from './NavLink';
-import type { ContractDocument } from 'types';
 import { useDatabase } from 'ui/contexts';
 
 export function QuickLinks() {
@@ -29,26 +28,6 @@ export function QuickLinks() {
             {' • '}
             <Link to={`/instantiate`}>Upload one</Link>
           </div>
-        )}
-      </div>
-      <div className="section liked-contracts">
-        <div className="header">Favorite Contracts</div>
-        {myContracts?.starred && myContracts?.starred.length > 0 ? (
-          myContracts.starred.map(({ isExistent, value }) => {
-            if (!value || !isExistent) {
-              return null;
-            }
-
-            const { name, address } = value as ContractDocument;
-
-            return (
-              <NavLink icon={DocumentTextIcon} key={address} to={`/contract/${address}`}>
-                {name}
-              </NavLink>
-            );
-          })
-        ) : (
-          <div className="none-yet">None yet</div>
         )}
       </div>
     </div>
