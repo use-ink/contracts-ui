39618257777ccf868d38a1695e43195292a43226
Andreea Eftene <hi@pixelle.ro>
2022-01-24 16:05:43 +0100

    remove jest related files and dependencies

--- __test-utils__/mocks/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2021 @paritytech/substrate-contracts-explorer authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export * from './keyring';
-export * from './contexts';
-export * from './contract';
-export * from './db';
-export * from './hooks';
