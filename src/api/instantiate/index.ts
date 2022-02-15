a13584722afd3028a18426f192c56b9e26dc0d28
Andreea Eftene <hi@pixelle.ro>
2022-02-15 14:56:45 +0100

    remove unused

--- src/api/instantiate/index.ts
+++ src/api/instantiate/index.ts
@@ -2,5 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from './instantiate';
-export * from './getInstanceFromEvents';
 export * from './createInstantiateTx';
--- src/types/ui/contract.ts
+++ src/types/ui/contract.ts
@@ -10,25 +10,12 @@ import type {
   RegistryError,
 } from '../substrate';
 
-export type RawParamValue = unknown | undefined;
-export type RawParamValueArray = (RawParamValue | RawParamValue[])[];
-export type RawParamValues = RawParamValue | RawParamValueArray;
-export interface RawParam {
-  isValid: boolean;
-  value: RawParamValues;
-}
-
 export interface ContractDryRunParams {
   contract: ContractPromise;
   message: AbiMessage;
   payment: BN;
   sender: KeyringPair;
   argValues?: Record<string, unknown>;
-}
-
-export interface ContractCallParams extends ContractDryRunParams {
-  gasLimit: BN;
-  dispatch: (action: ContractCallAction) => void;
 }
 
 export interface CallResult {
@@ -42,15 +29,3 @@ export interface CallResult {
   info?: Record<string, AnyJson>;
   time: number;
 }
-
-export interface ContractCallState {
-  isLoading: boolean;
-  isSuccess: boolean;
-  results: CallResult[];
-  error?: RegistryError;
-}
-
-export type ContractCallAction =
-  | { type: 'CALL_INIT'; payload: CallResult }
-  | { type: 'CALL_FINALISED'; payload: CallResult }
-  | { type: 'RESET' };
