99c153a0bfcc0979f8a93509c84534d3d5d38640
Keith Ingram <kwingram25@gmail.com>
2022-03-27 20:53:31 -0500

    add option type

---
+++ src/ui/components/form/Option.tsx
@@ -0,0 +1,64 @@
+// Copyright 2022 @paritytech/contracts-ui authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import React, { useCallback, useEffect } from 'react';
+import { Switch } from '../common/Switch';
+import { Input } from './Input';
+import { OrFalsy, Registry, SimpleSpread, TypeDef, ValidFormField } from 'types';
+import { useApi } from 'ui/contexts';
+import { getInitValue } from 'ui/util';
+import { useToggle } from 'ui/hooks/useToggle';
+import { NOOP } from 'api';
+
+type Props = SimpleSpread<React.InputHTMLAttributes<HTMLInputElement>, ValidFormField<unknown>> & {
+  component: React.ComponentType<ValidFormField<unknown> & React.HTMLAttributes<unknown>>;
+  registry: Registry;
+  typeDef: TypeDef;
+};
+
+export function Option({
+  component: Component,
+  onChange: _onChange,
+  registry,
+  typeDef,
+  value = null,
+}: Props) {
+  const { keyring } = useApi();
+
+  const [isSupplied, toggleIsSupplied] = useToggle();
+  const onChange = useCallback(
+    (value: OrFalsy<unknown>): void => {
+      if (!isSupplied) {
+        _onChange(null);
+
+        return;
+      }
+
+      _onChange(value);
+    },
+    [_onChange, isSupplied]
+  );
+
+  useEffect((): void => {
+    if (isSupplied && value === null) {
+      onChange(getInitValue(registry, keyring, typeDef.sub as TypeDef));
+    } else {
+      if (!isSupplied && value !== null) {
+        onChange(null);
+      }
+    }
+  }, [keyring, registry, onChange, value, isSupplied, typeDef.sub]);
+
+  return (
+    <div className="flex items-start">
+      {isSupplied ? (
+        <Component className="flex-1" onChange={onChange} value={value} />
+      ) : (
+        <Input className="flex-1" isDisabled onChange={NOOP} value="" placeholder="Do not supply" />
+      )}
+      <div className="flex justify-center items-center w-18 my-2.5">
+        <Switch value={isSupplied} onChange={toggleIsSupplied} />
+      </div>
+    </div>
+  );
+}
