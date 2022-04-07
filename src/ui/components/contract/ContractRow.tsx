ba7929db016a714504961a290fcb1e93f2fe82b1
Andreea Eftene <hi@pixelle.ro>
2022-04-07 21:56:06 +0200

    display message in contract list

--- src/ui/components/contract/ContractRow.tsx
+++ src/ui/components/contract/ContractRow.tsx
@@ -1,28 +1,46 @@
 // Copyright 2022 @paritytech/contracts-ui authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'styled-components';
 import { Link } from 'react-router-dom';
 import { Identicon } from '../account/Identicon';
 import { ContractDocument } from 'types';
 import { displayDate } from 'ui/util';
+import { useApi } from 'ui/contexts';
+import { getContractInfo } from 'api';
 
 interface Props {
   contract: ContractDocument;
 }
 
 export function ContractRow({ contract: { address, name, date } }: Props) {
+  const { api } = useApi();
+  const [isOnChain, setIsOnChain] = useState(true);
+
+  useEffect(() => {
+    getContractInfo(api, address)
+      .then(info => {
+        setIsOnChain(info ? true : false);
+      })
+      .catch(console.error);
+  }, [address, api]);
+
   return (
     <Link
       to={`/contract/${address}`}
-      className={`text-sm inline-flex cursor-pointer w-full hover:bg-gray-50 dark:hover:bg-elevation-1 border p-3 border-t-0 border-r-0 border-l-0 last:border-b-0 dark:text-white dark:border-gray-700 border-gray-200`}
+      className={`text-sm inline-flex items-center cursor-pointer w-full hover:bg-gray-50 dark:hover:bg-elevation-1 border p-3 border-t-0 border-r-0 border-l-0 last:border-b-0 dark:text-white dark:border-gray-700 border-gray-200`}
     >
       <Identicon size={18} value={address} className="pr-2" />
       <div className="w-36">{name}</div>
-      <div className="flex-grow text-gray-500 dark:text-gray-400">
-        {address.slice(0, 4)}...{address.slice(-4)}
-      </div>
+
+      {isOnChain ? (
+        <div className="flex-grow text-gray-500 dark:text-gray-400">
+          {address.slice(0, 4)}...{address.slice(-4)}
+        </div>
+      ) : (
+        <div className="text-gray-500 dark:text-gray-400 text-xs flex-grow mr-3">not on-chain</div>
+      )}
       <div className="w-14 text-gray-500 dark:text-gray-400">{displayDate(date)}</div>
     </Link>
   );
