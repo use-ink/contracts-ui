992ca9d4480072c729c77d81377e2b2195e2aa5e
Andreea Eftene <hi@pixelle.ro>
2022-04-13 00:31:46 +0200

    better styling for connection error

--- src/ui/components/common/ConnectionError.tsx
+++ src/ui/components/common/ConnectionError.tsx
@@ -11,26 +11,26 @@ function ContractsNodeHelp() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="mb-1 dark:text-gray-300 text-gray-500">
-        Make sure you are running a local{' '}
-        <a href="https://github.com/paritytech/substrate-contracts-node">
+      <div className="mb-1 text-gray-500">
+        <span>Make sure you are running a local </span>
+        <a href="https://github.com/paritytech/substrate-contracts-node" className="text-blue-400">
           substrate-contracts-node
         </a>
         .
       </div>
-      <p className="py-1 px-4 dark:bg-slate-800 bg-slate-200  dark:text-gray-400 text-gray-500 font-mono mb-6 text-sm rounded-sm">
+      <p className="py-1 px-4 dark:bg-slate-800 bg-slate-200 text-gray-500 font-mono mb-6 text-sm rounded-sm">
         susbtrate-contracts-node --dev
       </p>
-      <p className="dark:text-gray-300 text-gray-500">
-        Alternatively, connect to{' '}
+      <p className="text-gray-500">
+        <span>Alternatively, connect to </span>
         <a
           href="#"
           onClick={() => {
             navigate(`/?rpc=${RPC.CANVAS}`);
           }}
+          className="text-blue-400"
         >
-          {' '}
-          Canvas Testnet on Rococo
+          Canvas on Rococo
         </a>
       </p>
     </>
@@ -44,7 +44,7 @@ export function ConnectionError() {
     <div className="w-full overflow-y-auto overflow-x-hidden px-5 py-3 m-2 flex justify-center items-center">
       <div className=" w-7/12 h-60 border dark:border-gray-700 grid place-content-center justify-items-center">
         <ExclamationCircleIcon className="w-10 h-10 text-red-400 mb-1" />
-        <p className="dark:text-gray-300 text-gray-500 mb-6">Could not connect to {endpoint}</p>
+        <p className="text-gray-500 mb-6">Could not connect to {endpoint}</p>
         {endpoint === RPC.LOCAL && <ContractsNodeHelp />}
       </div>
     </div>
