fe502e2c1fff44d7fdc422f295e025f701b460af
Andreea Eftene <hi@pixelle.ro>
2022-06-29 11:00:30 +0200

    delete comments

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,32 +1,5 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-
-export {};
+import 'cypress-file-upload';
 
 declare global {
   namespace Cypress {
