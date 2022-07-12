491a32848969a6d82cfc0a415190a45d6035bc48
Andreea Eftene <hi@pixelle.ro>
2022-07-12 16:25:58 +0200

    improve extension check

--- src/api/loadAccounts.ts
+++ src/api/loadAccounts.ts
@@ -16,7 +16,8 @@ export const loadAccounts = (state: ApiState, dispatch: React.Dispatch<ApiAction
       systemChainType.isDevelopment || systemChainType.isLocal || isTestChain(systemChain);
     const extensions = await web3Enable('contracts-ui');
     let allAccounts = await web3Accounts();
-    if (isDevelopment || extensions.length > 0) {
+    const supportedExtension = extensions.find(e => e.name === 'polkadot-js');
+    if (isDevelopment || supportedExtension) {
       allAccounts = allAccounts.map(({ address, meta }) => ({
         address,
         meta: { ...meta, name: `${meta.name} (${meta.source})` },
