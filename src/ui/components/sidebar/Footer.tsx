c81e8647d942fe05be6124739c484c4df964d27a
erler <mail@michelerler.com>
2022-07-25 16:13:16 +0200

    Update Footer.tsx

--- src/ui/components/sidebar/Footer.tsx
+++ src/ui/components/sidebar/Footer.tsx
@@ -13,13 +13,13 @@ export function Footer() {
     <footer className="footer">
       <div>
         <a
-          className="flex content-center py-2 md:py-0 text-md md:text-xs font-medium dark:text-gray-500 text-gray-600 dark:hover:text-gray-300 hover:text-gray-400 cursor-pointer"
+          className="flex content-center items-center py-2 md:py-0 text-md md:text-xs font-medium dark:text-gray-500 text-gray-600 dark:hover:text-gray-300 hover:text-gray-400 cursor-pointer"
           onClick={() => setIsOpen(true)}
         >
           <ChatAltIcon className="h-4 w-4 mr-2" aria-hidden="true" />
           Help &amp; Feedback
         </a>
-        <Link className="flex py-2 md:py-0" to="/settings">
+        <Link className="flex items-center py-2 md:py-0" to="/settings">
           <CogIcon
             className="h-4 w-4 mr-2 dark:text-gray-500 dark:hover:text-gray-300 text-gray-600 hover:text-gray-400"
             aria-hidden="true"
