e8ac5d958b90dce681f7fe09fba35e93d506fd3b
Andreea Eftene <42985153+statictype@users.noreply.github.com>
2022-08-16 21:47:13 +0200

    Collect code coverage (#357)

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths(), eslintPlugin()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    eslintPlugin(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'cypress/'],
+      cypress: true,
+    }),
+  ],
   server: { port: 8081 },
   build: {
     rollupOptions: {
