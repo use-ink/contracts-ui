88564388c982abb7042b340db1570f8b48cbcd59
Andreea Eftene <hi@pixelle.ro>
2023-01-06 19:03:42 +0100

    remove unused hooks

--- src/ui/hooks/index.ts
+++ src/ui/hooks/index.ts
@@ -1,11 +1,9 @@
 // Copyright 2022 @paritytech/contracts-ui authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-export * from './useDebounce';
 export * from './useLocalStorage';
 export * from './useDbQuery';
 export * from './useWeight';
-export * from './useBlockTime';
 export * from './useBalance';
 export * from './useFormField';
 export * from './useArgValues';
