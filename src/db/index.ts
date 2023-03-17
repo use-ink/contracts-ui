f15b6d9dadcaabb9e8c90ca1399481f72e0104c8
peetzweg/ <phil.czek@gmail.com>
2023-03-17 13:15:30 +0100

    adds unique constraint(&) to `contract.address` and `codeBundle.codeHash`

--- src/db/index.ts
+++ src/db/index.ts
@@ -28,5 +28,66 @@ export class Database extends Dexie {
       codeBundles: '++id, codeHash, name, date',
       contracts: '++id, address, codeHash, name, date',
     });
+
+    // Stores same as version 1, but with removes duplicate codeBundles and contracts during upgrade
+    this.version(2)
+      .stores({
+        codeBundles: '++id, codeHash, name, date',
+        contracts: '++id, address, codeHash, name, date',
+      })
+      .upgrade(async transaction => {
+        const bundleCodeHashes = new Set<string>();
+        const duplicateBundles: Array<CodeBundleDocument['id']> = [];
+
+        const contractAddresses = new Set<string>();
+        const duplicateContracts: Array<ContractDocument['id']> = [];
+
+        // Iterate over all code bundles and collect id's of duplicate code hashes
+        await Promise.all([
+          transaction.table('codeBundles').each((codeBundle: CodeBundleDocument) => {
+            if (bundleCodeHashes.has(codeBundle.codeHash)) {
+              duplicateBundles.push(codeBundle.id);
+            } else {
+              bundleCodeHashes.add(codeBundle.codeHash);
+            }
+          }),
+          transaction.table('contracts').each((contract: ContractDocument) => {
+            if (contractAddresses.has(contract.address)) {
+              duplicateContracts.push(contract.id);
+            } else {
+              contractAddresses.add(contract.address);
+            }
+          }),
+        ]);
+
+        return Promise.all([
+          transaction.table('codeBundles').bulkDelete(duplicateBundles),
+          transaction.table('contracts').bulkDelete(duplicateContracts),
+        ]);
+      });
+
+    // Adds unique constraint on codeHash.codeHash and contracts.address
+    // Needs new version because upgrade and new unique constraint throws an error,
+    // Two step process works fine though
+    this.version(3).stores({
+      codeBundles: '++id, &codeHash, name, date',
+      contracts: '++id, &address, codeHash, name, date',
+    });
+  }
+
+  /**
+   * Can be used to populate the initial v1 database with mock data.
+   */
+  _populateWithMockDataV1() {
+    if (this.verno !== 1) return;
+
+    // Populate is only called if the database did not exist
+    this.on('populate', async () => {
+      const CodeBundlesMock = (await import('./mocks/v1/codeBundles.json')).default;
+      await this.table('codeBundles').bulkAdd(CodeBundlesMock);
+
+      const ContractsMock = (await import('./mocks/v1/contracts.json')).default;
+      await this.table('contracts').bulkAdd(ContractsMock);
+    });
   }
 }
