e0bfbb1866e5e7d5db4d70c0763fe3cf3f4e2a88
Andreea Eftene <42985153+statictype@users.noreply.github.com>
2023-06-19 10:49:59 +0200

    feat: hash input  (#455)

--- src/ui/components/form/InputBytes.tsx
+++ src/ui/components/form/InputBytes.tsx
@@ -3,46 +3,53 @@
 
 import React, { useCallback, useState } from 'react';
 import { hexToU8a, compactAddLength } from '@polkadot/util';
-import { Input } from './Input';
+import { InputHex } from './InputHex';
 import { ArgComponentProps } from 'types';
-import { classes } from 'helpers';
 
 type Props = ArgComponentProps<Uint8Array>;
 
-export function InputBytes({ onChange, className }: Props): React.ReactElement<Props> {
-  const [displayValue, setDisplayValue] = useState('0000');
+type Validation = { isValid: boolean; message?: string };
+
+function validate(value: string): Validation {
+  if (value.length % 2 !== 0) {
+    return {
+      isValid: false,
+      message: 'A trailing zero will be added. Please input an even number of bytes.',
+    };
+  }
+  return {
+    isValid: true,
+    message: '',
+  };
+}
+
+export function InputBytes({
+  onChange,
+  className,
+  defaultValue,
+}: Props): React.ReactElement<Props> {
+  const [{ isValid, message }, setValidation] = useState<Validation>({ isValid: true });
+
   const handleChange = useCallback(
     (d: string) => {
-      const regex = /^(0x|0X)?[a-fA-F0-9]+$/;
-      if (!d || regex.test(d)) {
-        setDisplayValue(d);
-        try {
-          const raw = hexToU8a(`0x${d}`);
-          onChange(compactAddLength(raw));
-        } catch (e) {
-          console.error(e);
-        }
+      const validation = validate(d);
+      setValidation(validation);
+      try {
+        const raw = hexToU8a(`0x${d}`);
+        onChange(compactAddLength(raw));
+      } catch (e) {
+        console.error(e);
       }
     },
     [onChange]
   );
 
   return (
-    <>
-      <div className="relative flex w-full items-center">
-        <span className="absolute left-3 text-sm text-gray-400">0x</span>
-        <Input
-          className={classes('flex-1 pl-8', className)}
-          onChange={handleChange}
-          placeholder="hexadecimal representation of Bytes"
-          type="text"
-          value={displayValue}
-        />
-      </div>
-      <div className="mt-1 basis-full text-xs text-red-600">
-        {displayValue.length % 2 !== 0 &&
-          'A trailing zero will be added. Please input an even number of bytes.'}
-      </div>
-    </>
+    <InputHex
+      defaultValue={defaultValue?.toString()}
+      onChange={handleChange}
+      className={className}
+      error={!isValid ? message : undefined}
+    />
   );
 }
