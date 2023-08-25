1d951f65ac9db4991744ea1169bf33810bc45148
Gabo Esquivel <contact@gaboesquivel.com>
2023-08-25 09:53:36 -0600

    chore: remove duplicated copyright

--- src/ui/pages/Homepage.tsx
+++ src/ui/pages/Homepage.tsx
@@ -4,9 +4,6 @@
 import { Contracts, HelpBox, Statistics } from '../components/homepage';
 import { RootLayout } from 'ui/layout';
 
-// Copyright 2022 @paritytech/contracts-ui authors & contributors
-// SPDX-License-Identifier: GPL-3.0-only
-
 export function Homepage() {
   return (
     <RootLayout
