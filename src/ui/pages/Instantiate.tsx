a3746c4e5ed0750cb188e96cffae20b6937565f0
peetzweg/ <phil.czek@gmail.com>
2023-11-21 11:27:32 +0100

    use `RootLayout` to improve page consistency

--- src/ui/pages/Instantiate.tsx
+++ src/ui/pages/Instantiate.tsx
@@ -2,47 +2,41 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { Link, useParams } from 'react-router-dom';
-import { InstantiateContextProvider } from 'ui/contexts';
+import { RootLayout } from '../layout/RootLayout';
 import { Wizard } from 'ui/components/instantiate';
+import { InstantiateContextProvider } from 'ui/contexts';
 
 export function Instantiate() {
   const { codeHash: codeHashUrlParam } = useParams<{ codeHash: string }>();
 
   return (
-    <div className="m-2 w-full overflow-y-auto overflow-x-hidden px-5 py-3">
-      <div className="grid gap-5 md:grid-cols-12">
-        <div className="p-4 md:col-span-9">
-          <div className="space-y-1 border-b border-gray-200 pb-6 dark:border-gray-800">
-            <h1>
-              {codeHashUrlParam
-                ? 'Instantiate Contract from Code Hash'
-                : 'Upload and Instantiate Contract'}
-            </h1>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              {codeHashUrlParam ? (
-                <>
-                  You can upload and instantiate new contract code{' '}
-                  <Link className="text-blue-500" to="/instantiate">
-                    here
-                  </Link>
-                  .
-                </>
-              ) : (
-                <>
-                  You can instantiate a new contract from an existing code bundle{' '}
-                  <Link className="text-blue-500" to="/hash-lookup">
-                    here
-                  </Link>
-                  .
-                </>
-              )}
-            </p>
-          </div>
-        </div>
-      </div>
+    <RootLayout
+      heading={
+        codeHashUrlParam ? 'Instantiate Contract from Code Hash' : 'Upload and Instantiate Contract'
+      }
+      help={
+        codeHashUrlParam ? (
+          <>
+            You can upload and instantiate new contract code{' '}
+            <Link className="text-blue-500" to="/instantiate">
+              here
+            </Link>
+            .
+          </>
+        ) : (
+          <>
+            You can instantiate a new contract from an existing code bundle{' '}
+            <Link className="text-blue-500" to="/hash-lookup">
+              here
+            </Link>
+            .
+          </>
+        )
+      }
+    >
       <InstantiateContextProvider>
         <Wizard />
       </InstantiateContextProvider>
-    </div>
+    </RootLayout>
   );
 }
