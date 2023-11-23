9ee2984af24c1bc350152a9bdfd7f8500a400b86
peetzweg/ <839848+peetzweg@users.noreply.github.com>
2023-11-23 16:26:16 +0100

    feat: shows `free` balance of contracts (#530)

---
+++ src/lib/formatBalance.ts
@@ -0,0 +1,39 @@
+// Copyright 2022 @paritytech/contracts-ui authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { Balance } from 'types';
+
+type FormattingOptions = {
+  decimals: number;
+  symbol: string | undefined;
+  fractionDigits: number;
+};
+
+const DEFAULT_OPTIONS: FormattingOptions = {
+  decimals: 12,
+  fractionDigits: 2,
+  symbol: undefined,
+};
+
+export const formatBalance = (balance: Balance, partialOptions?: Partial<FormattingOptions>) => {
+  const options: FormattingOptions = { ...DEFAULT_OPTIONS, ...partialOptions };
+
+  if (options.decimals < 0) throw new Error('Decimals must be positive');
+  if (options.fractionDigits < 0) throw new Error('Fraction digits must be positive');
+  if (options.decimals < options.fractionDigits)
+    throw new Error('Decimals must be greater than fraction digits');
+
+  const balanceString = balance.toString();
+  const integerDigits = balanceString.split('');
+  const fractionDigits = integerDigits.splice(-options.decimals);
+
+  const fractionalPart = fractionDigits.join('').padStart(options.decimals, '0');
+  const integerPart = integerDigits.length ? integerDigits.join('') : '0';
+
+  return (
+    Intl.NumberFormat('en-US', { maximumFractionDigits: 0 }).format(BigInt(integerPart)) +
+    '.' +
+    fractionalPart.toString().slice(0, options.fractionDigits).padEnd(options.fractionDigits, '0') +
+    (options.symbol ? ` ${options.symbol}` : '')
+  );
+};
---
+++ src/ui/components/common/ObservedBalance.tsx
@@ -0,0 +1,34 @@
+// Copyright 2022 @paritytech/contracts-ui authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { useEffect, useState } from 'react';
+import { formatBalance } from 'lib/formatBalance';
+import { Balance } from 'types';
+import { useApi } from 'ui/contexts';
+
+export const ObservedBalance = ({ address }: { address: string }) => {
+  const { api, tokenDecimals, tokenSymbol } = useApi();
+
+  const [balance, setBalance] = useState<null | Balance>(null);
+  useEffect(() => {
+    const unsubscribePromise = api.query.system.account(address, result => {
+      setBalance(result.data.free);
+    });
+
+    return () => {
+      unsubscribePromise
+        .then(unsubscribe => {
+          unsubscribe();
+        })
+        .catch(console.error);
+    };
+  }, [address, api]);
+
+  if (!balance) return null;
+
+  return formatBalance(balance, {
+    decimals: tokenDecimals,
+    symbol: tokenSymbol,
+    fractionDigits: 2,
+  });
+};
