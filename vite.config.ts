24aebb0a669d54b4fc342e610a935d73851623e4
peetzweg/ <phil.czek@gmail.com>
2024-02-06 16:35:45 +0100

    makes test work in ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 // Copyright 2022 @paritytech/contracts-ui authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
-
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import tsConfigPaths from 'vite-tsconfig-paths';
-import eslintPlugin from '@nabla/vite-plugin-eslint';
 import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
@@ -12,7 +11,6 @@ export default defineConfig({
   plugins: [
     react(),
     tsConfigPaths(),
-    eslintPlugin(),
     istanbul({
       include: 'src/*',
       exclude: ['node_modules', 'cypress/'],
