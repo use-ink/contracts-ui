c68a143152ae0fa7701bacf76a7f29b1786ace81
peetzweg/ <phil.czek@gmail.com>
2024-02-20 14:18:50 +0100

    implements search and display of known abis/metadata when adding onchain contracts

---
+++ src/ui/hooks/useStoredMetadata.tsx
@@ -0,0 +1,23 @@
+// Copyright 2022-2024 @paritytech/contracts-ui authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { useDbQuery } from '.';
+import { Abi } from 'types';
+import { useDatabase } from 'ui/contexts';
+
+export const useStoredMetadata = () => {
+  const { db } = useDatabase();
+
+  return useDbQuery(async () => {
+    const collectedAbis = new Set();
+    const storedAbis: Abi[] = [];
+    await db.contracts.each(contract => {
+      if (collectedAbis.has(contract.codeHash)) return;
+
+      const abi = new Abi(contract.abi);
+      storedAbis.push(abi);
+      collectedAbis.add(contract.codeHash);
+    });
+    return storedAbis;
+  }, [db]);
+};
