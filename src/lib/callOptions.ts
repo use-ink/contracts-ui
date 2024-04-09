1a6b445af17c6597141b5cb6d77cb3e55fed9eba
peetzweg/ <phil.czek@gmail.com>
2024-04-09 14:27:00 +0200

    removes unused code

--- src/lib/callOptions.ts
+++ src/lib/callOptions.ts
@@ -1,9 +1,8 @@
 // Copyright 2022-2024 @paritytech/contracts-ui authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { randomAsU8a } from './util';
 import { BN_ZERO } from './bn';
-import { UIStorageDeposit, ContractCallOutcome, BN, WeightV2, AbiParam, Registry } from 'types';
+import { AbiParam, BN, ContractCallOutcome, Registry, UIStorageDeposit, WeightV2 } from 'types';
 
 export function decodeStorageDeposit(
   storageDeposit: ContractCallOutcome['storageDeposit'],
@@ -68,4 +67,3 @@ export function transformUserInput(
     return value;
   });
 }
-const encoder = new TextEncoder();
