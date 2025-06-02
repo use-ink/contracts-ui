95cae9e9f00d9ea4e1911d2f7585f54510847532
AlexD10S <alexfraga10@gmail.com>
2025-06-02 12:14:37 +0200

    chore: undo changes in snapshots.js

--- snapshots.js
+++ snapshots.js
@@ -1,3 +1,6 @@
+// Copyright 2022-2024 use-ink/contracts-ui authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 module.exports = {
   __version: '13.13.3',
   'Storage Types Contract': {
